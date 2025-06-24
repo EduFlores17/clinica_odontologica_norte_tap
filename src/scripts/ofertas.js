d9d6fd63f73218d048e6b42100073c16afc26ecd
EduFlores17 <fedu17t@gmail.com>
2025-06-23 19:37:46 -0600

    C2.6 - Galeria y modificación del carrusel

--- src/scripts/ofertas.js
+++ src/scripts/ofertas.js
@@ -3,24 +3,24 @@ export const ofertas = [
     id: 1,
     titulo: "Resinas en buen precio.",
     descripcion: "Aprovecha este mes con el tratamiento de 6 resinas a $2650.00 MXN .",
-    imagen: "/img_ofertas/promo.png",
+    imagen: "/img_ofertas/of1.jpeg",
   },
   {
     id: 2,
     titulo: "Pide tu cita.",
     descripcion: "Al número de telefono: 962 127 8373",
-    imagen: "/img_ofertas/promo2.png",
+    imagen: "/img_ofertas/of2.jpeg",
   },
   {
     id: 3,
     titulo: "Blanqueamiento dental + Limpieza dental.",
     descripcion: "Todo esto en un precio de $1899.00 MXN",
-    imagen: "/img_ofertas/promo3.png",
+    imagen: "/img_ofertas/of3.mp4",
   },
   {
     id: 4,
     titulo: "Tu salud, es primero.",
     descripcion: "2 resinas + limpieza dental + Cepillado prolifactica + Aplicación de fluor por $1299.00",
-    imagen: "/img_ofertas/promo4.png",
+    imagen: "/img_ofertas/of4.mp4",
   },
 ];
