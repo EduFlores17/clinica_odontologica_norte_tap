db42af91cec4e46fe57a03bfc3b05f5b8841e181
Temovlog17 <fedu17t@gmail.com>
2025-06-26 12:28:58 -0600

    C2.8.1 - Diseño de conocenos

---
+++ src/scripts/doctores.js
@@ -0,0 +1,44 @@
+export const doctores = [
+  {
+    id: 1,
+    nombre: "Dr. Jordi Lopez",
+    especialidad: "Especialista en ortodoncia",
+    cedula: "12345678",
+    imagen: "/img_doctores/doctor1.jpeg"
+  },
+  {
+    id: 2,
+    nombre: "Dr. Jonathan Sanchez",
+    especialidad: "Implantología dental",
+    cedula: "87654321",
+    imagen: "/img_doctores/doctor2.jpeg"
+  },
+  {
+    id: 3,
+    nombre: "Dra. Mariana Torres",
+    especialidad: "Endodoncia y estética dental",
+    cedula: "23456789",
+    imagen: "/img_doctores/doctor3.jpeg"
+  },
+  {
+    id: 4,
+    nombre: "Dr. Luis Gómez",
+    especialidad: "Cirugía maxilofacial",
+    cedula: "34567890",
+    imagen: "/img_doctores/doctor4.jpeg"
+  },
+  {
+    id: 5,
+    nombre: "Dra. Alejandra Ruiz",
+    especialidad: "Odontopediatría",
+    cedula: "45678901",
+    imagen: "/img_doctores/doctor5.jpeg"
+  },
+  {
+    id: 6,
+    nombre: "Dr. Enrique Morales",
+    especialidad: "Periodoncia",
+    cedula: "56789012",
+    imagen: "/img_doctores/doctor6.jpeg"
+  }
+];
